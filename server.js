d99f5611f4b3a28a97427aaaf67228f092aedab6
Dennis de Greef <github@link0.net>
2015-10-25 20:40:37 +0100

    Refactored to only one indentation per method and added comments

--- server.js
+++ server.js
@@ -27,31 +27,73 @@ var config   = require('./config');
 var mqttUri  = 'mqtt://' + config.mqtt.hostname + ':' + config.mqtt.port;
 var client   = mqtt.connect(mqttUri);
 
-client.on('connect', function () {
+client.on('connect', onMqttConnection);
+
+/**
+ * When the MQTT connection is established, this function is called
+ */
+function onMqttConnection() {
     log("Connected to MQTT: " + mqttUri);
-    log("Starting to listen on tcp://" + config.socket.host + ':' + config.socket.port);
-    net.createServer(function(sock) {
-        sock.on('data', function(streamData) {
-            var data = streamData.toString();
 
-            var json = validateJSON(data.replace(/^\s+|\s+$/g, ''));
-            if (json && json.secret == config.socket.secret) {
+    net.createServer(onSocketServer).listen(config.socket.port, config.socket.host);
+}
 
-                if (json.topic == undefined || json.message == undefined) {
-                    failure(sock, "Valid json and secret, but no topic or message set");
-                }
+/**
+ * When the socket server is created, this function is called
+ *
+ * @param socket
+ */
+function onSocketServer(socket) {
+    log("Listening on tcp://" + config.socket.host + ':' + config.socket.port);
+    socket.on('data', handleMessage);
+}
 
-                client.publish(json.topic, json.message, {}, function() {
-                    success(sock, json);
-                });
+/**
+ * Handle method for messages on the socket
+ *
+ * @param streamData
+ * @returns {*}
+ */
+function handleMessage(streamData) {
+    // streamData is a Buffer, we wan't to parse a string
+    var data = streamData.toString();
 
-            } else {
-                failure(sock, "Invalid json or secret");
-            }
-        });
-    }).listen(config.socket.port, config.socket.host);
-});
+    // Validate the JSON after 'trimming'
+    var json = validateJSON(data.replace(/^\s+|\s+$/g, ''));
 
+    // Json must be valid
+    if (!json) {
+        return failure("Invalid JSON");
+    }
+
+    // Secret must match that if the config
+    if (json.secret != config.socket.secret) {
+        return failure("Incorrect secret");
+    }
+
+    // JSON must contain a topic for the MQTT publish
+    if (json.topic == undefined) {
+        return failure("Missing topic in JSON");
+    }
+
+    // JSON must contain a message for the MQTT publish
+    if (json.message == undefined) {
+        return failure("Missing message in JSON");
+    }
+
+    // Publish the message onto the MQTT connection
+    client.publish(json.topic, json.message, {}, function() {
+        return success(sock, json);
+    });
+
+}
+
+/**
+ * Called when call is succesfull
+ *
+ * @param socket
+ * @param json
+ */
 function success(socket, json) {
 
     // Remove secret from object before logging
@@ -63,6 +105,12 @@ function success(socket, json) {
     });
 }
 
+/**
+ * Called when something is wrong with handling the message
+ *
+ * @param socket
+ * @param reason
+ */
 function failure(socket, reason) {
     log("Failure from " + socket.remoteAddress + ": " + reason);
     writeResult(socket, {
@@ -70,14 +118,30 @@ function failure(socket, reason) {
     });
 }
 
+/**
+ * Writes the result back to the original socket
+ *
+ * @param socket
+ * @param json
+ */
 function writeResult(socket, json) {
     socket.write(JSON.stringify(json) + "\n");
 }
 
+/**
+ * Like console.log, but prepends a time
+ * @param message
+ */
 function log(message) {
     console.log((new Date().toISOString()) + ": " + message);
 }
 
+/**
+ * Returns the given body if it is valid JSON, otherwise null
+ *
+ * @param body
+ * @returns string|null
+ */
 function validateJSON(body) {
     try {
         return JSON.parse(body);
