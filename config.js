ce9106c2d338f1cda9c749cf61dfb62daf5cad5f
Eva Lok <squidstock@hotmail.com>
2015-11-02 01:23:49 -0700

    improved readability

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    debug: process.env.DEBUG || false,
+    debug: process.env.DEBUG                  || false,
     socket: {
-        host: process.env.SOCKET_HOST   || '127.0.0.1',
-        port: process.env.SOCKET_PORT   || 1445,
-        secret: process.env.SOCKET_SECRET || 'youshouldchangethis'
+        host: process.env.SOCKET_HOST         || '127.0.0.1',
+        port: process.env.SOCKET_PORT         || 1445,
+        secret: process.env.SOCKET_SECRET     || 'youshouldchangethis'
     },
     mqtt: {
         namespace: process.env.MQTT_NAMESPACE || '',
-        hostname: process.env.MQTT_HOSTNAME  || '127.0.0.1',
-        port: process.env.MQTT_PORT      || 1883
+        hostname: process.env.MQTT_HOSTNAME   || '127.0.0.1',
+        port: process.env.MQTT_PORT           || 1883
     }
 };
